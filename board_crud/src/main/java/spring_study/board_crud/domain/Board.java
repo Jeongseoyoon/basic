bde9e659a2a3291890c3e24152be16bd1fa3b1b1
Jeongseoyoon <jsydjsy@gmail.com>
2024-03-15 16:38:37 +0900

    first

---
+++ board_crud/src/main/java/spring_study/board_crud/domain/Board.java
@@ -0,0 +1,33 @@
+//package spring_study.board_crud.domain;
+// # src/main/java/domain/Board.java
+
+package spring_study.board_crud.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data // @Getter @Setter
+@Table(name = "board")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id, title"})
+public class Board {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Board(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+}
